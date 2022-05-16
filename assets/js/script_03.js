4370f3d480139d3853f09a6ae7f2792fd1dfc0dd
jemm2008 <jemm2008@gmail.com>
2022-05-16 13:16:14 -0400

    Se mejoró la función de validación

--- assets/js/script_03.js
+++ assets/js/script_03.js
@@ -3,13 +3,15 @@ let formulariox = document.getElementById('sumar_restar')
 formulariox.addEventListener("click" , (event) => {
     event.preventDefault()
     let id_btn_press = event.target.id
-    let valor_01 = parseInt(document.getElementById("valor1").value)
-    let valor_02 = parseInt(document.getElementById("valor2").value)
+    let val_01 = document.getElementById("valor1").value
+    let val_02 = document.getElementById("valor2").value
+    let valor_01 = parseInt(val_01)
+    let valor_02 = parseInt(val_02)
     document.getElementById("tituloErrores").innerHTML = ""
     document.getElementById("val_1").innerHTML = ""
     document.getElementById("val_2").innerHTML = ""
     document.getElementById("val_2menor").innerHTML = ""
-    if (validar(valor_01) && validar(valor_02)){
+    if (validar(val_01) && validar(val_02)){
         if(id_btn_press == "btn_sumar") {
             document.querySelector(".resultado").innerHTML = valor_01 + valor_02
         }
@@ -22,12 +24,12 @@ formulariox.addEventListener("click" , (event) => {
             document.getElementById("val_2menor").innerHTML = `Valor 2 (${valor_02}) de preferencia menor a Valor 1  (${valor_01})`
         }
     }
-    if (validar(valor_01) == false) {
+    if (validar(val_01) == false) {
         document.querySelector(".resultado").innerHTML = ""
         document.getElementById("tituloErrores").innerHTML = "Error!!"
         document.getElementById("val_1").innerHTML = "Valor 1 debe ser un número (máx 8 dígitos)."
     } 
-    if (validar(valor_02) == false) {
+    if (validar(val_02) == false) {
         document.querySelector(".resultado").innerHTML = ""
         document.getElementById("tituloErrores").innerHTML = "Error!!"
         document.getElementById("val_2").innerHTML = "Valor 2 debe ser un número (máx 8 dígitos)."
